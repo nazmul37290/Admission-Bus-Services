a975cfac827740e18ff7536c66296283305ec9a2
Nazmul Haque <nazmulnjm@gmail.com>
2024-12-28 13:07:04 +0600

    update global error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 const app: Application = express();
 
 app.use(express.json());
@@ -11,4 +12,9 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Bus is running too fast!! 🚌");
 });
 
+app.use((err, req, res, next) => {
+  return res.status(500).json({
+    mesage: "something wrong",
+  });
+});
 export default app;
