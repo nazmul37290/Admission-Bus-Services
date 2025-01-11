fe4e521c3503ddfd8f5702edf713763cb032b93f
Nazmul Haque <nazmulnjm@gmail.com>
2025-01-11 09:57:45 +0600

    updated vercel .json

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bus-services-client.vercel.app"], // Frontend URL
+    origin: [
+      "http://localhost:5173",
+      "https://bus-services-client.vercel.app,",
+    ], // Frontend URL
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
     credentials: true, // Enable cookies and authentication headers (if using sessions)
