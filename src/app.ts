274c760fac58da0e23157d1bab730907dddaa4f4
Nazmul Haque <nazmulnjm@gmail.com>
2025-04-19 11:29:39 +0600

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,21 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://bus-services-client.vercel.app",
   "http://busservice-frontend-xxk9hz-b91cde-159-223-41-30.traefik.me",
+  "https://busservice-frontend-xxk9hz-b91cde-159-223-41-30.traefik.me",
 ];
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      console.log("🔍 Origin request received from:", origin);
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
